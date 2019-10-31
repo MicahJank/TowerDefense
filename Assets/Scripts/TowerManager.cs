9007989c87d0d25fdc94820fde15fd8c0d31c27c
MicahJank <MicahJank@users.noreply.github.com>
2019-10-31 18:40:48 -0500

    tower buttons will now log which tower button was pressed when clicked

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerManager : Singleton<TowerManager> {
+
+    private TowerButton towerBtnPressed;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -11,5 +14,10 @@ public class TowerManager : Singleton<TowerManager> {
     // Update is called once per frame
     void Update() {
         
+    }
+
+    public void selectedTower(TowerButton towerSelected) {
+        towerBtnPressed = towerSelected;
+        Debug.Log("Pressed:" + towerBtnPressed.gameObject);
     }
 }
